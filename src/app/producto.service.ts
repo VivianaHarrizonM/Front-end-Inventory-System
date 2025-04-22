ef0ac5aef7f35963f46b6abd1c54531feb586d6d
Viviana Harrizon <viviana.harrizon@gmail.com>
2025-04-21 19:38:20 -0600

    Edit env

--- src/app/producto.service.ts
+++ src/app/producto.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Producto } from './producto';
+import { environment } from './env/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
-  private UrlBase = "http://localhost:8080/inventario-app/productos";
+  private UrlBase = `${environment.apiUrl}/productos`;
   private clienteHTTP = inject(HttpClient);
 
   obtenerProductosLista(): Observable<Producto[]>{
